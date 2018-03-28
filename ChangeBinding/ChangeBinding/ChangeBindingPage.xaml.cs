4fb1aa73b777cbff236aa027dda78450f241b981
zybug <zy@zybug.com>
2018-03-28 20:58:34 +0800

    增加binding 测试

---
+++ ChangeBinding/ChangeBinding/ChangeBindingPage.xaml.cs
@@ -0,0 +1,43 @@
+﻿using Xamarin.Forms;
+
+namespace ChangeBinding
+{
+    public partial class ChangeBindingPage : ContentPage
+    {
+
+        private MainViewModel viewModel;
+        public ChangeBindingPage()
+        {
+            InitializeComponent();
+            Title = "Segmented Control";
+
+            this.BindingContext = viewModel = new MainViewModel();
+        }
+
+        void Handle_ValueChanged(object sender, SegmentedControl.FormsPlugin.Abstractions.ValueChangedEventArgs e)
+        {
+            var i = e.NewValue;
+            viewModel.ChangeModelType(i);
+
+            //switch (e.NewValue)
+            //{
+                //case 0:
+                //    SegContent.Children.Clear();
+                //    SegContent.Children.Add(new Label() { Text = "Items tab selected" });
+                //    break;
+                //case 1:
+                //    SegContent.Children.Clear();
+                //    SegContent.Children.Add(new Label() { Text = "Notes tab selected" });
+                //    break;
+                //case 2:
+                //    SegContent.Children.Clear();
+                //    SegContent.Children.Add(new Label() { Text = "Approvers tab selected" });
+                //    break;
+                //case 3:
+                    //SegContent.Children.Clear();
+                    //SegContent.Children.Add(new Label() { Text = "Attachments tab selected" });
+                    //break;
+            //}
+        }
+    }
+}
---
+++ ChangeBinding/ChangeBinding/MainViewModel.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.ObjectModel;
+
+namespace ChangeBinding
+{
+    public class MainViewModel
+    {
+        public ObservableCollection<Person> People { private set; get; } = new ObservableCollection<Person>();
+
+        public MainViewModel()
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Person person = new Person("zhangyi");
+                People.Add(person);
+            }
+        }
+
+        public void ChangeModelType(int i)
+        {
+            People.Clear();
+            if (i == 0) 
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    Person person = new Person("zhangyi");
+                    People.Add(person);
+                }
+            }
+
+            if (i == 1)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    Person person = new Person("zhanger");
+                    People.Add(person);
+                }
+            }
+
+            if (i == 2)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    Person person = new Person("zhangsan");
+                    People.Add(person);
+                }
+            }
+
+            if (i == 3)
+            {
+                for (int j = 0; j < 10; j++)
+                {
+                    Person person = new Person("zhangsi");
+                    People.Add(person);
+                }
+            }
+        }
+    }
+
+    public class Person
+    {
+        public string Name { set; get; }
+
+        public Person(string name)
+        {
+            Name = name;
+        }
+
+    }
+}
